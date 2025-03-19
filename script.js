faa772b48351f6cfef5a77df8956f442fe5cb641
AnaSophiafl <134711925+AnaSophiafl@users.noreply.github.com>
2025-03-19 08:52:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-document.addEventListener('DOMContentLoaded',funcion (){
+ocument.addEventListener('DOMContentLoaded',funcion (){
 const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
 let tamanhoAtualFonte = 1;
 
 aumentaFonteBotao.addEventListener('click', funccion(){
     tamanhoAtualFonte += 0.1;
     document.body.style.fontSize = '§{tamanhoAtualFonte}rem'
+
+})
+
+diminuiFonteBotao.addEventListener('click', funccion(){
+    tamanhoAtualFonte -= 0.1;
+    document.body.style.fontSize = '§{tamanhoAtualFonte}rem'
+    
 })
 
 
 })
-
-
-
